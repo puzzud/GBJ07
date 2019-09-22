f96012806c2dd5c8d556e0e177eaaaa9e7148a3a
puzzud <puzzud@gmail.com>
2019-09-22 10:02:03 -0400

    Added logic to track which cells each block is in.

--- Scripts/Stage.gd
+++ Scripts/Stage.gd
@@ -11,16 +11,23 @@ var board = Array()
 
 func _ready():
 	board = buildBoard()
+	#printBoard(board)
 
 func buildBoard() -> Array:
 	var board = Array()
 	
+	var levelIndex = -1
 	for level in $Contents.get_children():
+		levelIndex += 1
+		var boardLevel = initializeBoardLevel()
+		
 		for entity in level.get_children():
 			if entity is Block:
-				var cellCoordinates = getCellCoordinatesFromBlock(entity)
-				print(entity.name + ":" + str(cellCoordinates.x) + "," + str(cellCoordinates.y))
-	
+				if !insertEntityIntoBoardLevel(boardLevel, levelIndex, entity):
+					printerr("Failed to insert \"" + entity.name + "\" into level #" + str(boardLevel) + ".")
+		
+		board.push_back(boardLevel)
+		
 	return board
 
 func getCellCoordinatesFromBlock(block) -> Vector2:
@@ -31,3 +38,55 @@ func getCellCoordinatesFromBlock(block) -> Vector2:
 	coordinates.y = int((block.position.y + 8) / cellHeight)
 	
 	return coordinates
+
+func initializeBoardLevel():
+	var boardLevel = Array()
+	
+# warning-ignore:unused_variable
+	for y in range(0, boardHeight):
+		var boardLevelRow = Array()
+		
+# warning-ignore:unused_variable
+		for y in range(0, boardWidth):
+			boardLevelRow.push_back(null)
+		
+		boardLevel.push_back(boardLevelRow)
+	
+	return boardLevel
+
+func insertEntityIntoBoardLevel(boardLevel: Array, levelIndex: int, entity) -> bool:
+	var cellCoordinates = getCellCoordinatesFromBlock(entity)
+	var x = cellCoordinates.x
+	var y = cellCoordinates.y
+	
+	if x < 0 || x >= boardWidth:
+		return false
+	
+	if y < 0 || y >= boardHeight:
+		return false
+	
+	boardLevel[y][x] = entity
+	
+	#print("Level #" + str(levelIndex) + ": " + entity.name + ":" + str(x) + "," + str(y))
+	
+	return true
+
+func printBoard(board: Array):
+	print("Board contents:")
+	
+	var levelIndex = -1
+	for level in board:
+		levelIndex += 1
+		
+		print("Level#" + str(levelIndex) + ":")
+		for row in level:
+			var rowString = ""
+			
+			for cell in row:
+				var cellCharacter = ' '
+				if cell is Block:
+					cellCharacter = 'X'
+				
+				rowString += cellCharacter
+			
+			print(rowString)
